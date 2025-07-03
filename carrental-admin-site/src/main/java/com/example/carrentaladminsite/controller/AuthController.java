bcd06573e49f99368e23f2eab67cdb54546714bc
1Ir-is <mhuynk1005@gmail.com>
2025-07-03 17:55:59 +0700

    sth

--- carrental-admin-site/src/main/java/com/example/carrentaladminsite/controller/AuthController.java
+++ carrental-admin-site/src/main/java/com/example/carrentaladminsite/controller/AuthController.java
@@ -1,12 +1,28 @@
 package com.example.carrentaladminsite.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
     @GetMapping("/login")
-    public String login() {
+    public String login(
+            @RequestParam(value = "timeout", required = false) String timeout,
+            Model model,
+            Authentication authentication
+    ) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            String className = authentication.getPrincipal().getClass().getSimpleName();
+            if (!"String".equals(className) || !"anonymousUser".equals(authentication.getPrincipal())) {
+                return "redirect:/admin";
+            }
+        }
+        if (timeout != null) {
+            model.addAttribute("message", "Bạn đã bị đăng xuất do không hoạt động quá lâu.");
+        }
         return "login";
     }
 }
